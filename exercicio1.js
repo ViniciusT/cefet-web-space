366a096fec16135a225cf53ad45092800f9d1a8f
Vinicius Teodoro <vinicius.teodoro@enacom.com.br>
2017-04-11 12:02:04 -0300

    Tp pronto

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,52 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+var p1 = false;
+var p2 = false;
+var p3 = false;
+
+function insertClass(id) {
+    var d;
+    var b;
+    if(id == 'p1'){
+        d = document.getElementById(id);
+        b = document.getElementById('buttonp1');
+        if(p1 ==  false){
+            d.className += " expandido";
+            b.textContent = "-"
+        }
+        else{
+            d.classList.remove("expandido");
+            b.textContent = "+"
+        }
+        p1 = !p1;
+    }
+    else if(id =='p2'){
+        d = document.getElementById(id);
+        b = document.getElementById("button" + id);
+        if(p2 ==  false){
+            d.className += " expandido";
+            b.textContent = "-"
+        }
+        else{
+            d.classList.remove("expandido");
+            b.textContent = "+"
+        }
+        p2 = !p2;
+    }
+    else{
+        d = document.getElementById(id);
+        b = document.getElementById("button" + id);
+        if(p3 ==  false){
+
+            d.className += " expandido";
+            b.textContent = "-"
+        }
+        else{
+            d.classList.remove("expandido");
+            b.textContent = "+"
+        }
+        p3 = !p3;
+    }
+
+
+}
--- exercicio2.js
+++ exercicio2.js
@@ -1,11 +1,33 @@
 // Faça o exercício da galeria de imagens aqui
 // Este arquivo ainda não está incluído no arquivo HTML
-
+var atual = 0;
 const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
-  todasAsImagens = [
-    'philae-parts.jpg',
-    'philae-rosetta.jpg',
-    'philae-separation.jpg',
-    'philae-67-picture.jpg',
-    'philae-collecting.jpg'
-  ];
+    todasAsImagens = [
+        'philae-parts.jpg',
+        'philae-rosetta.jpg',
+        'philae-separation.jpg',
+        'philae-67-picture.jpg',
+        'philae-collecting.jpg'
+    ];
+
+    function proximo() {
+        if(atual == 4){
+            atual = 0;
+        }
+        else{
+            atual++;
+        }
+
+        document.getElementById("slide").src = servidorDasImagens + todasAsImagens[atual];
+
+    }
+    function anterior() {
+        if(atual == 0){
+            atual = 4;
+        }
+        else{
+            atual--;
+        }
+
+        document.getElementById("slide").src = servidorDasImagens + todasAsImagens[atual];
+    }
